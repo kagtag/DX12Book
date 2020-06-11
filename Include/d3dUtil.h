228f14619f2e7688a895fb91577a3ab471adcab9
MicSong <issongmc@outlook.com>
2020-06-12 00:31:09 +0800

    Add box app

--- Include/d3dUtil.h
+++ Include/d3dUtil.h
@@ -5,15 +5,59 @@
 #include <d3d12.h>
 #include <dxgi1_4.h>
 #include <wrl.h>
+#include <d3dcompiler.h>
 
 #include <DirectXMath.h>
 #include <DirectXPackedVector.h>
 #include <string>
 #include <vector>
+#include <memory>
+#include <DirectXCollision.h>
+#include <DirectXColors.h>
+#include <array>
 
 
 #include "d3dx12.h"
+#include <unordered_map>
 
+using Microsoft::WRL::ComPtr;
+
+
+inline std::wstring AnsiToWString(const std::string& str)
+{
+	WCHAR buffer[512];
+	MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, buffer, 512);
+	return std::wstring(buffer);
+}
+
+
+class d3dUtil
+{
+public:
+
+
+	static UINT CalcConstantBufferByteSize(UINT byteSize)
+	{
+		// Constant buffers must be a multiple of the minimum hardware
+		// allocation size (usually 256 bytes). So round up to nearest 
+		// multiple of 256
+		return (byteSize + 255) & ~255;
+	}
+
+	static ComPtr<ID3D12Resource> CreateDefaultBuffer(
+		ID3D12Device* device,
+		ID3D12GraphicsCommandList* cmdList,
+		const void* initData,
+		UINT64 byteSize,
+		ComPtr<ID3D12Resource>& uploadBuffer);
+
+	static ComPtr<ID3DBlob> CompileShader(
+		const std::wstring& filename,
+		const D3D_SHADER_MACRO* defines,
+		const std::string& entrypoint,
+		const std::string& target
+	);
+};
 
 
 class DxException
@@ -31,12 +75,73 @@ public:
 };
 
 
-inline std::wstring AnsiToWString(const std::string& str)
+
+// Defines a subrange of geometry in a MeshGeometry. This is for when multiple 
+// geometries are stored in 1 vertex and index buffer.
+struct SubmeshGeometry
 {
-	WCHAR buffer[512];
-	MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, buffer, 512);
-	return std::wstring(buffer);
-}
+	UINT IndexCount = 0;
+	UINT StartIndexLocation = 0;
+	INT BaseVertexLocation = 0;
+
+	DirectX::BoundingBox Bounds;
+};
+
+struct MeshGeometry
+{
+	std::string Name;
+
+	// System memory copies. Use Blobs because the vertex/index format can be generic
+	// It is up to the client to cast appropriately
+	ComPtr<ID3DBlob> VertexBufferCPU = nullptr;
+	ComPtr<ID3DBlob> IndexBufferCPU = nullptr;
+
+	ComPtr<ID3D12Resource> VertexBufferGPU = nullptr;
+	ComPtr<ID3D12Resource> IndexBufferGPU = nullptr;
+
+	ComPtr<ID3D12Resource> VertexBufferUploader = nullptr;
+	ComPtr<ID3D12Resource> IndexBufferUploader = nullptr;
+
+	// Data about the buffers
+	UINT VertexByteStride = 0;
+	UINT VertexBufferByteSize = 0;
+
+	DXGI_FORMAT IndexFormat = DXGI_FORMAT_R16_UINT;
+	UINT IndexBufferByteSize = 0;
+
+	// A MeshGeometry may store multiple geometries in 1 vertex/index buffer
+	// Use this container to define the Submesh geometries so we can draw the 
+	// Submeshes individually
+	std::unordered_map<std::string, SubmeshGeometry> DrawArgs;
+
+	D3D12_VERTEX_BUFFER_VIEW VertexBufferView() const
+	{
+		D3D12_VERTEX_BUFFER_VIEW vbv;
+		vbv.BufferLocation = VertexBufferGPU->GetGPUVirtualAddress();
+		vbv.StrideInBytes = VertexByteStride;
+		vbv.SizeInBytes = VertexBufferByteSize;
+
+		return vbv;
+	}
+
+	D3D12_INDEX_BUFFER_VIEW IndexBufferView() const
+	{
+		D3D12_INDEX_BUFFER_VIEW ibv;
+		ibv.BufferLocation = IndexBufferGPU->GetGPUVirtualAddress();
+		ibv.Format = IndexFormat;
+		ibv.SizeInBytes = IndexBufferByteSize;
+
+		return ibv;
+	}
+
+	void DisposeUploaders()
+	{
+		VertexBufferUploader = nullptr;
+		IndexBufferUploader = nullptr;
+	}
+};
+
+
 
 #ifndef ThrowIfFailed
 #define ThrowIfFailed(x)\
